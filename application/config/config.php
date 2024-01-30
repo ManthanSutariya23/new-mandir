62cfd9162ff8682ba350798f492909ce19ec931f
ManthanSutariya23 <86596725+ManthanSutariya23@users.noreply.github.com>
2024-01-30 21:43:02 +0000

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/mandir/';
+$config['base_url'] = 'https://manthansutariya23.github.io/new-mandir/';
 
 /*
 |--------------------------------------------------------------------------
